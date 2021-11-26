a4e6153a06e49b533cd32091b5cf9043b11bb2a4
Stefan Scheller <ste.scheller@gmail.com>
2021-11-26 22:52:37 +0100

    switched to vscode+ninja for building and developing

--- examples/VgaMonitor/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cmake -G "Eclipse CDT4 - Unix Makefiles" \
-    -DCMAKE_ECLIPSE_VERSION=4.21.0 \
-    -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -S ./ \
-    -B ../../build/examples/VgaMonitorExample
